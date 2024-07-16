309fd6c218e34e16b90b5282fa81c69b18f7046a
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-07-16 23:25:53 +0600

    Added naw file

--- functions.php
+++ functions.php
@@ -1,9 +1,6 @@
 <?php
-
-/**
- * Essential theme supports
- * */
-function theme_setup()
+//  Essential theme supports
+function hexShop_theme_setup()
 {
     /** automatic feed link*/
     add_theme_support('automatic-feed-links');
@@ -21,42 +18,36 @@ function theme_setup()
     /** HTML5 support **/
     add_theme_support('html5', array('comment-list', 'comment-form', 'search-form', 'gallery', 'caption'));
 
-    /** refresh widgest **/
+    /** refresh widgets **/
     add_theme_support('customize-selective-refresh-widgets');
 
-    /** custom background **/
-    $bg_defaults = array(
-        'default-image'          => '',
-        'default-preset'         => 'default',
-        'default-size'           => 'cover',
-        'default-repeat'         => 'no-repeat',
-        'default-attachment'     => 'scroll',
+    // register_nav_menu
+    register_nav_menus(
+        array(
+            'main-menu' => __('Main Menu', 'hexShop'),
+            'footer-menu' => __('Footer Menu', 'hexShop'),
+        )
     );
-    add_theme_support('custom-background', $bg_defaults);
 
-    /** custom header **/
-    $header_defaults = array(
-        'default-image'          => '',
-        'width'                  => 300,
-        'height'                 => 60,
-        'flex-height'            => true,
-        'flex-width'             => true,
-        'default-text-color'     => '',
-        'header-text'            => true,
-        'uploads'                => true,
-    );
-    add_theme_support('custom-header', $header_defaults);
-
-    /** custom log **/
-    add_theme_support('custom-logo', array(
-        'height'      => 60,
-        'width'       => 400,
-        'flex-height' => true,
-        'flex-width'  => true,
-        'header-text' => array('site-title', 'site-description'),
-    ));
+    // remove_theme_support
+    remove_theme_support('widgets-block-editor');
 }
-add_action('after_setup_theme', 'theme_setup');
+add_action('after_setup_theme', 'hexShop_theme_setup');
+
+// hexShop_copyright_option
+function hexShop_copyright_option()
+{
+    $hexShop_copyright = get_theme_mod('hexShop_copyright_id', __('Copyright &copy; 2024 Company Name - Design: Template Mo', 'hexShop'));
+?>
+    <p><?php echo wp_kses_post($hexShop_copyright); ?></p>
+<?php
+}
+function hexShop_footer()
+{
+    get_template_part('/inc/template-parts/footer/footer');
+}
 
 // nec files
 include_once('inc/common/scripts.php');
+include_once('inc/hexShop-kirki.php');
+include_once('inc/common/template-function.php');
